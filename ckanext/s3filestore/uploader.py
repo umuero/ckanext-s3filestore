70010f51f30dc49ce07e5655ab8a148bd94d40af
Lupco Kotev <lupco.kotev@keitaro.info>
2017-10-03 11:29:45 +0200

    Remove duplicate error checking for bucket existence

--- ckanext/s3filestore/uploader.py
+++ ckanext/s3filestore/uploader.py
@@ -48,27 +48,15 @@ class BaseS3Uploader(object):
                                         region_name=self.region)
         s3 = session.resource('s3', endpoint_url=self.host_name,
                               config=botocore.client.Config(signature_version=self.signature))
-        bucket = s3.Bucket(bucket_name)
         try:
-            if s3.Bucket(bucket.name) in s3.buckets.all():
-                log.info('Bucket {0} found!'.format(bucket_name))
-
-            else:
-                log.warning(
-                    'Bucket {0} could not be found, attempting to create it...'.format(bucket_name))
-                try:
-                    bucket = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
-                        'LocationConstraint': 'us-east-1'})
-                    log.info(
-                        'Bucket {0} succesfully created'.format(bucket_name))
-                except botocore.exceptions.ClientError as e:
-                    log.warning('Could not create bucket {0}: {1}'.format(
-                        bucket_name, str(e)))
+            bucket = s3.Bucket(bucket_name)
+            # Test if bucket exists
+            s3.meta.client.head_bucket(Bucket=bucket.name)
+            log.info('Bucket {0} found!'.format(bucket_name))
         except botocore.exceptions.ClientError as e:
             error_code = int(e.response['Error']['Code'])
             if error_code == 404:
-                log.warning('Bucket {0} could not be found, ' +
-                            'attempting to create it...'.format(bucket_name))
+                log.warning('Bucket {0} could not be found, attempting to create it...'.format(bucket_name))
                 try:
                     bucket = s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
                         'LocationConstraint': self.region})
